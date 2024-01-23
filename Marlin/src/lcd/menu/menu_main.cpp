ecb5f3ebdfe85b7a97cbed8e922630cfbb7cf964
BeatrizMoreira30 <bia30.albano@gmail.com>
2024-01-23 00:40:27 -0300

    Correcao parar

--- Marlin/src/lcd/menu/menu_main.cpp
+++ Marlin/src/lcd/menu/menu_main.cpp
@@ -236,7 +236,7 @@ void menu_main() {
   BACK_ITEM(MSG_INFO_SCREEN);
 
   if (planner.movesplanned() > 0)
-      GCODES_ITEM(MSG_BUTTON_STOP, F("G80\nG28"));
+      GCODES_ITEM(MSG_BUTTON_STOP, F("G80\nG28 Z\nG28 X Y\nM18 S2\n"));
   else{
 
   #if ENABLED(SDSUPPORT)
